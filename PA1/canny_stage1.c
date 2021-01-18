49556aa88e89ad518fd5a99e603525d8d53701ee
kmakmichael <m_kmak@u.pacific.edu>
2021-01-18 13:50:47 -0800

    basic setup

---
+++ PA1/canny_stage1.c
@@ -0,0 +1,14 @@
+/*
+    Michael Kmak
+    ECPE 251 - High-Performance Computing
+    PA1 - Canny Edge Detector
+
+    quick description...
+*/
+
+#include "stdio.h"
+
+int main() {
+
+    return 0;
+}
