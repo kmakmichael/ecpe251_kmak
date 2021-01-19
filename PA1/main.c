bb0376311b5130170308c63f987df80786baf510
kmakmichael <m_kmak@u.pacific.edu>
2021-01-19 15:44:32 -0800

    parse arguments

--- PA1/main.c
+++ PA1/main.c
@@ -3,12 +3,28 @@
     ECPE 251 - High-Performance Computing
     PA1 - Canny Edge Detector
 
+    usage: ./canny_stage1 <image path> <sigma>
+
     quick description...
 */
 
 #include "stdio.h"
+#include "stdlib.h"
+#include "inttypes.h"
 
-int main() {
+int main(int argc, char *argv[]) {
 
+    char *img_path;
+    float sigma;
+
+    if (argc != 3) {
+        fprintf(stderr, "usage: canny_stage1 <image path> <sigma>\n");
+        return -1;
+    }
+    sigma = atof(argv[2]);
+    if (sigma <= 0) {
+        fprintf(stderr, "invalid sigma: %s\n", argv[2]);
+    }
+    
     return 0;
 }
