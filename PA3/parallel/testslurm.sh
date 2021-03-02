bfc96ad7dbea988f551c991366bdc10c68130261
m_kmak <m_kmak@u.pacific.edu>
2021-03-02 03:11:53 -0800

    p3: slurm files

--- PA3/parallel/testslurm.sh
+++ PA3/parallel/testslurm.sh
@@ -3,5 +3,5 @@
 #SBATCH --job-name=PA3        ### Job Name
 #SBATCH --time=03:00:00       ### WallTime
 #SBATCH --nodes=4             ### Number of Nodes
-#SBATCH --tasks-per-node=4   ### Number of tasks (MPI processes)
+#SBATCH --tasks-per-node=8   ### Number of tasks (MPI processes)
 srun ./canny images/Lenna_org_2048.pgm 0.6 4  > testruns.csv
