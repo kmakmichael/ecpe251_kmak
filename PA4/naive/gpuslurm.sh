1fe981568518b9a846d805aaad5fb94479bea2cb
mkmak <m_kmak@u.pacific.edu>
2021-03-28 02:28:10 -0700

    pa4: proper slurm file

--- PA4/naive/gpuslurm.sh
+++ PA4/naive/gpuslurm.sh
@@ -5,6 +5,16 @@
 #SBATCH --nodes=1               ### Number of Nodes
 #SBATCH --tasks-per-node=1      ### Number of tasks
 
-for((i=0;i<1;i++)) do
-	srun ./canny ~/lennas/Lenna_org_4096.pgm 0.6
+rm Serial.csv # clear previous results
+
+for sig in 0.6 1.1
+do
+    for ((i=1024;i<=8192;i=i*2)) do
+        for((j=0;j<5;j++)) do
+	        srun ./canny ~/lennas/Lenna_org_$i.pgm $sig >> Serial.csv
+        done
+    done
+    for((j=0;j<5;j++)) do
+	    srun ./canny ~/lennas/Lenna_org_10240.pgm $sig >> Serial.csv
+    done
 done
