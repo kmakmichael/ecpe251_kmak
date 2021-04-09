d49339441fc1b57e0470113caaf914955d257547
mkmak <m_kmak@u.pacific.edu>
2021-04-09 00:39:54 -0700

    adjust slurm scripts

--- PA5/gpu/gpuslurm.sh
+++ PA5/gpu/gpuslurm.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 ##SBATCH --partition=compute    ### Partition
-#SBATCH --job-name=PA4          ### Job Name
+#SBATCH --job-name=PA5_G        ### Job Name
 #SBATCH --time=03:00:00         ### WallTime
 #SBATCH --nodes=1               ### Number of Nodes
 #SBATCH --tasks-per-node=1      ### Number of tasks
 
-rm optimized.csv # clear previous results
+rm GPU.csv # clear previous results
 
-for sig in 0.6 1.1
+for i in 256 512 1024 2048 4096 8192 10240
 do
-    for ((i=1024;i<=8192;i=i*2)) do
-        for((j=0;j<5;j++)) do
-	        srun ./canny ~/lennas/Lenna_org_$i.pgm $sig >> optimized.csv
-        done
-    done
-    for((j=0;j<5;j++)) do
-	    srun ./canny ~/lennas/Lenna_org_10240.pgm $sig >> optimized.csv
+    for((j=0;j<30;j++)) do
+	    srun ./canny ~/lennas/Lenna_org_$i.pgm 0.6 >> GPU.csv
     done
 done
--- PA5/gpu/test.sh
+++ PA5/gpu/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ##SBATCH --partition=compute	### Partition
-#SBATCH --job-name=PA4		    ### Job Name
+#SBATCH --job-name=PA5_T	    ### Job Name
 #SBATCH --time=03:00:00     	### WallTime
 #SBATCH --nodes=1          	    ### Number of Nodes
 #SBATCH --tasks-per-node=1 	    ### Number of tasks
 
-srun ./canny ~/lennas/Lenna_org_4096.pgm 0.6 > tout.csv
+srun ./canny ~/lennas/Lenna_org_2048.pgm 0.6 > tout.csv
