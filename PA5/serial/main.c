0d47ecec07c194a4ff1e1b8209e2be6dbaa45be2
mkmak <m_kmak@u.pacific.edu>
2021-04-09 02:55:35 -0700

    pa5: update serial code

--- PA5/serial/main.c
+++ PA5/serial/main.c
@@ -1,11 +1,10 @@
 /*
     Michael Kmak
     ECPE 251 - High-Performance Computing
-    PA1 - Canny Edge Detector
+    PA3 - Canny Edge Detector
 
-    usage: ./canny_stage1 <image path> <sigma>
+    usage: ./canny <image path> <sigma> <num threads>
 
-    quick description...
 */
 
 #include "stdio.h"
@@ -13,8 +12,12 @@
 #include "inttypes.h"
 #include "math.h"
 #include "sys/time.h"
+#include "omp.h"
 
+#include "sort.h"
 #include "image_template.h"
+
+#define timing_mode 1
 
 // structs
 typedef struct {
@@ -31,15 +34,15 @@ typedef struct {
 // functions
 void gaussian_kern(kern_s *kern, float sigma, float a);
 void gaussian_deriv(kern_s *kern, float sigma, float a);
+void kerninit(kern_s *kern);
 void img_prep(const img_s *orig, img_s *cpy);
 void h_conv(img_s *in_img, img_s *out_img, const kern_s *kern);
 void v_conv(img_s *in_img, img_s *out_img, const kern_s *kern);
 void suppression(img_s *direction, img_s *magnitude, img_s *out_img);
-void hyst(img_s *img, float t_high, float t_low);
+void hysteresis(img_s *img, float t_high, float t_low);
 void edge_linking(img_s *hyst, img_s *edges);
 float timecalc(struct timeval start, struct timeval end);
-
-int sortcomp(const void *e1, const void *e2);
+int comp(const void *a, const void *b);
 
 int main(int argc, char *argv[]) {
 
@@ -49,48 +52,75 @@ int main(int argc, char *argv[]) {
     img_s hori;
     img_s magnitude;
     img_s direction;
-    kern_s kern;
-    struct timeval readstart, compstart, conv, mag, sup, sort, doublethresh, edge, end;
+    img_s supp;
+    img_s hyst;
+    size_t numthreads;
+    kern_s h_kern;
+    kern_s h_deriv;
+    kern_s v_kern;
+    kern_s v_deriv;
+    struct timeval start, compstart, conv, mag, sup, sort, doublethresh, edge, compend, end;
     float sigma;
     float a;
 
     if (argc != 3) {
-        fprintf(stderr, "usage: canny_stage1 <image path> <sigma>\n");
+        fprintf(stderr, "usage: canny <image path> <sigma>\n");
         return -1;
     }
     sigma = atof(argv[2]);
     if (sigma <= 0) {
         fprintf(stderr, "invalid sigma: %s\n", argv[2]);
+        return -1;
     }
+    numthreads = 1; //atoi(argv[3]);
+    /*if (numthreads <= 0) {
+        fprintf(stderr, "invalid number of threads: %s\n", argv[3]);
+        return -1;
+    }
+    if (numthreads > omp_get_num_procs()) {
+        fprintf(stderr, "trying to use more threads than processors\n");
+        return -1;
+    }
+    omp_set_num_threads(numthreads);
+    */
 
     // begin time
-    gettimeofday(&readstart, NULL);
+    gettimeofday(&start, NULL);
     read_image_template(argv[1], &image.data, &image.width, &image.height);
     img_prep(&image, &temp);
     img_prep(&image, &vert);
     img_prep(&image, &hori);
     img_prep(&image, &magnitude);
     img_prep(&image, &direction);
-
-    // kernel initialization
+    img_prep(&image, &supp);
+    img_prep(&image, &hyst);
+    
     a = round(2.5 * sigma - 0.5);
-    kern.w = 2 * a + 1;
-    kern.data = (float*) calloc(kern.w, sizeof(float));
+    h_kern.w = 2 * a + 1;
+    h_kern.data = (float*) calloc(h_kern.w, sizeof(float));
+    v_kern.w = 2 * a + 1;
+    v_kern.data = (float*) calloc(v_kern.w, sizeof(float));
+    h_deriv.w = 2 * a + 1;
+    h_deriv.data = (float*) calloc(h_deriv.w, sizeof(float));
+    v_deriv.w = 2 * a + 1;
+    v_deriv.data = (float*) calloc(v_deriv.w, sizeof(float));
 
-    // begin time
+    gaussian_kern(&h_kern, sigma, a);
+    gaussian_kern(&v_kern, sigma, a);
+    gaussian_deriv(&h_deriv, sigma, a);
+    gaussian_deriv(&v_deriv, sigma, a);
+
     gettimeofday(&compstart, NULL);
 
     // horizontal
-    gaussian_kern(&kern, sigma, a);
-    h_conv(&image, &temp, &kern);
-    gaussian_deriv(&kern, sigma, a);
-    h_conv(&temp, &hori, &kern);
+    h_conv(&image, &temp, &h_kern);
+    h_conv(&temp, &hori, &h_deriv);
 
     // vertical
-    gaussian_kern(&kern, sigma, a);
-    v_conv(&image, &temp, &kern);
-    gaussian_deriv(&kern, sigma, a);
-    v_conv(&temp, &vert, &kern);
+    v_conv(&image, &temp, &v_kern);
+    v_conv(&temp, &vert, &v_deriv);
+
+    gettimeofday(&conv, NULL);
 
     // direction and magnitude
     for(size_t i = 0; i < image.height * image.width; i++) {
@@ -99,25 +129,64 @@ int main(int argc, char *argv[]) {
     for(size_t i = 0; i < image.height * image.width; i++) {
         direction.data[i] = atan2(hori.data[i], vert.data[i]);
     }
-    write_image_template("direction.pgm", direction.data, direction.width, direction.height);
-    write_image_template("magnitude.pgm", magnitude.data, magnitude.width, magnitude.height);
 
-    // re-use vert as suppression and hori as hysteresis to save some memory so my VM can handle the bigger images
-    suppression(&direction, &magnitude, &vert);
-    write_image_template("suppression.pgm", vert.data, vert.width, vert.height);
+    gettimeofday(&mag, NULL);
 
-    memcpy(temp.data, vert.data, sizeof(float) * vert.height * vert.width);
-    qsort(temp.data, temp.width * temp.height, sizeof(float), sortcomp);
-    float t_high = temp.data[(size_t) (temp.height * temp.width * 0.9)];
-    float t_low = t_high / 5.0;
-    hyst(&vert, t_high, t_low);
-    edge_linking(&vert, &hori);
-    write_image_template("hysteresis.pgm", hori.data, hori.width, hori.height);
+    suppression(&direction, &magnitude, &supp);
+
+    gettimeofday(&sup, NULL);
+
+    memcpy(temp.data, supp.data, sizeof(float) * supp.height * supp.width);
+    qsort(temp.data, temp.width*temp.height, sizeof(float), comp);
+
+    float t_high = temp.data[(int) (temp.height * temp.width * 0.9)];
+    float t_low = t_high * 0.2;
+   
+    write_image_template("sorted.pgm", temp.data, temp.width, temp.height);
+ 
+    gettimeofday(&sort, NULL);
+
+    memcpy(hyst.data, supp.data, sizeof(float) * supp.height * supp.width);
+    hysteresis(&hyst, t_high, t_low);
+
+    gettimeofday(&doublethresh, NULL);
+
+    edge_linking(&temp, &hyst);
+
+    gettimeofday(&edge, NULL);
 
     // stop time
+    gettimeofday(&compend, NULL);
+    
+    write_image_template("direction.pgm", direction.data, direction.width, direction.height);
+    write_image_template("magnitude.pgm", magnitude.data, magnitude.width, magnitude.height);
+    write_image_template("suppression.pgm", supp.data, supp.width, supp.height);
+    write_image_template("hysteresis.pgm", hyst.data, hyst.width, hyst.height);
+
     gettimeofday(&end, NULL);
 
-    printf("%d, %.1f, %.1f, %.1f\n", image.height, sigma, timecalc(compstart, end), timecalc(readstart, end));
+    printf("%d, %.1f, %zu, %.1f, %.1f\n", 
+        image.height, 
+        sigma, 
+        numthreads, 
+        timecalc(compstart, end), 
+        timecalc(start, end)
+    );
+
+    if (timing_mode) {
+        printf("indx=%d, hi=%03f, lo=%0.3f\n",(int)(0.9 * image.height * image.width), t_high, t_low); 
+        printf("\ncomp\tconv\tmag\tsup\tsort\tdt\tedge\ttotal\n");
+        printf("%.1f\t%.1f\t%.1f\t%.1f\t%.1f\t%.1f\t%.1f\t%.1f\n",
+            timecalc(compstart, compend), // comp_time
+            timecalc(compstart, conv), // conv_time
+            timecalc(conv, mag), // mag_time
+            timecalc(mag, sup), // sup_time
+            timecalc(sup, sort), // sort_time
+            timecalc(sort, doublethresh), // doublethresh_time
+            timecalc(doublethresh, edge), // edge_time
+            timecalc(start, end) // total_time
+        );
+    }
 
     free(image.data);
     free(temp.data);
@@ -125,7 +194,12 @@ int main(int argc, char *argv[]) {
     free(hori.data);
     free(magnitude.data);
     free(direction.data);
-    free(kern.data);
+    free(hyst.data);
+    free(supp.data);
+    free(h_kern.data);
+    free(v_kern.data);
+    free(h_deriv.data);
+    free(v_deriv.data);
     return 0;
 }
 
@@ -146,7 +220,6 @@ void gaussian_kern(kern_s *kern, float sigma, float a) {
     float sum = 0;
 
     kern->data = (float*) calloc(kern->w, sizeof(float));
-
     for (size_t i = 0; i < kern->w; i++) {
         kern->data[i] = exp((-1.0 * (i-a) * (i-a)) / (2.0 * sigma * sigma));
         sum += kern->data[i];
@@ -178,7 +251,7 @@ void gaussian_deriv(kern_s *kern, float sigma, float a) {
 
 void h_conv(img_s *in_img, img_s *out_img, const kern_s *kern) {
     size_t bounds = in_img->width * in_img->height;
-    int i_off = 0;
+    int i_off = 0; // private when ||ized
     for (size_t i = 0; i < bounds; i++) {
         float sum = 0;
         for( size_t k = 0; k < kern->w; k++) {
@@ -197,7 +270,7 @@ void h_conv(img_s *in_img, img_s *out_img, const kern_s *kern) {
 
 void v_conv(img_s *in_img, img_s *out_img, const kern_s *kern) {
     size_t bounds = in_img->width * in_img->height;
-    int i_off = 0;
+    int i_off = 0; // private when ||ized
     for (size_t i = 0; i < bounds; i++) {
         float sum = 0;
         for( size_t k = 0; k < kern->w; k++) {
@@ -217,7 +290,7 @@ void suppression(img_s *direction, img_s *magnitude, img_s *supp) {
     size_t width = magnitude->width;
     size_t btm_right = width + 1;
     size_t btm_left = width - 1;
-    float theta;
+    float theta; // private when ||ized
     for (size_t i = 0; i < bounds; i++) {
         theta = direction->data[i];
         if (theta < 0) {
@@ -230,14 +303,12 @@ void suppression(img_s *direction, img_s *magnitude, img_s *supp) {
             if (i >= width) {
                 if (Gxy[i] < Gxy[i - width]) {
                     supp->data[i] = 0;
-                    continue;
                 }
             }
             // bottom
             if (i < bounds - width) {
                 if (Gxy[i] < Gxy[i + width]) {
                     supp->data[i] = 0;
-                    continue;
                 }
             }
         } else if (theta > 22.5 && theta <= 67.5) {
@@ -245,14 +316,12 @@ void suppression(img_s *direction, img_s *magnitude, img_s *supp) {
             if (i >= width && i % width > 0) {
                 if (Gxy[i] < Gxy[i - btm_right]) {
                     supp->data[i] = 0;
-                    continue;
                 }
             }
             // bottomright
             if (i < bounds - width && i % width < width-1) {
                 if (Gxy[i] < Gxy[i + btm_right]) {
                     supp->data[i] = 0;
-                    continue;
                 }
             }
         } else if (theta > 67.5 && theta <= 112.5) {
@@ -260,14 +329,12 @@ void suppression(img_s *direction, img_s *magnitude, img_s *supp) {
             if (i % width > 0) {
                 if (Gxy[i] < Gxy[i - 1]) {
                     supp->data[i] = 0;
-                    continue;
                 }
             }
             // right
             if (i % width < width-1) {
                 if (Gxy[i] < Gxy[i + 1]) {
                     supp->data[i] = 0;
-                    continue;
                 }
             }
         } else if (theta > 112.5 && theta <= 157.5) {
@@ -275,14 +342,12 @@ void suppression(img_s *direction, img_s *magnitude, img_s *supp) {
             if (i >= width && i % width < width-1) {
                 if (Gxy[i] < Gxy[i - btm_left]) {
                     supp->data[i] = 0;
-                    continue;
                 }
             }
             // bottomleft
             if (i < bounds - width && i % width > 0) {
                 if (Gxy[i] < Gxy[i + btm_left]) {
                     supp->data[i] = 0;
-                    continue;
                 }
             }
         }
@@ -290,7 +355,7 @@ void suppression(img_s *direction, img_s *magnitude, img_s *supp) {
     #undef Gxy
 }
 
-void hyst(img_s *img, float t_high, float t_low) {
+void hysteresis(img_s *img, float t_high, float t_low) {
     size_t bounds = img->height * img-> width;
     for(size_t i = 0; i < bounds; i++) {
         if (img->data[i] >= t_high) {
@@ -310,63 +375,55 @@ void edge_linking(img_s *hyst, img_s *edges) {
     size_t btm_left = width - 1;
     for (size_t i = 0 ; i < bounds; i++) {
         if(hyst->data[i] == 125) {
+            edges->data[i] = 0;
             // topleft
             if (i >= width && i % width > 0) {
                 if (hyst->data[i - btm_right] == 255) {
                     edges->data[i] = 255;
-                    continue;
                 }
             }
             // top
             if (i >= width) {
                 if (hyst->data[i - width] == 255) {
                     edges->data[i] = 255;
-                    continue;
                 }
             }
             // topright
             if (i >= width && i % width < width-1) {
                 if (hyst->data[i - btm_left] == 255) {
                     edges->data[i] = 255;
-                    continue;
                 }
             }
             // left
             if (i % width > 0) {
                 if (hyst->data[i - 1] == 255) {
                     edges->data[i] = 255;
-                    continue;
                 }
             }
             // right
             if (i % width > width-1) {
                 if (hyst->data[i + 1] == 255) {
                     edges->data[i] = 255;
-                    continue;
                 }
             }
             // bottomleft
             if (i < bounds - width && i % width > 0) {
                 if (hyst->data[i + btm_left] == 255) {
                     edges->data[i] = 255;
-                    continue;
                 }
             }
             // bottom
             if (i < bounds - width) {
                 if (hyst->data[i + width] == 255) {
                     edges->data[i] = 255;
-                    continue;
                 }
             }
             // bottomright
             if (i < bounds - width && i % width < width-1) {
                 if (hyst->data[i + btm_right] == 255) {
                     edges->data[i] = 255;
-                    continue;
                 }
             }
-            edges->data[i] = 0;
         } else {
             edges->data[i] = hyst->data[i];
         }
@@ -378,8 +435,10 @@ float timecalc(struct timeval start, struct timeval end) {
     return ns / 1000.0;
 }
 
-int sortcomp(const void *e1, const void *e2) {
-    float f1 = *(float *) e1;
-    float f2 = *(float *) e2;
-    return (f1 > f2) - (f1 < f2);
+
+int comp (const void * a, const void * b)
+{
+    float fa = *(const float*) a;
+    float fb = *(const float*) b;
+    return (fa > fb) - (fa < fb);
 }
