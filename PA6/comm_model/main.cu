6a730f85693e16003771a4bcd2b433913115e7b1
mkmak <m_kmak@u.pacific.edu>
2021-04-14 19:48:20 -0700

    pa6: memcpy logic

--- PA6/comm_model/main.cu
+++ PA6/comm_model/main.cu
@@ -7,7 +7,7 @@
 
     copies a randomly-filled float array between
     the CPU and GPU in both directions. size of
-    this array is 2^N bytes, where N is given as
+    this array is 2^n floats, where n is given as
     a program argument. outputs time taken in 
     the following format:
         size H2D D2H
@@ -16,15 +16,24 @@
 #include <cuda.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <sys/time.h>
 
 #define GPU_NO 1 // 85 % 4
 
+float timecalc(struct timeval start, struct timeval end) {
+    float us = (end.tv_sec*1000000 + end.tv_usec) - (start.tv_sec*1000000 + start.tv_usec);
+    return us / 1000.0;
+}
 
 int main(int argc, char *argv[]) {
     
     float *h_arr;
     float *d_arr;
+    size_t size;
     size_t n;
+
+    struct timeval start, end;
+    float htod, dtoh;
 
     // argparse
     if (argc != 2) {
@@ -41,6 +50,39 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    printf("Creating array of floats. size 2^%zu=%f bytes, containing %f floats\n", n, exp2((float)n), exp2((float)n)/sizeof(float));
-    n = exp2((float)n);
+    size = exp2((float)n) * sizeof(float);
+    h_arr = (float *) malloc(size);
+    cudaMalloc((void **)&d_arr, size);
+
+    gettimeofday(&start, NULL);
+    cudaMemcpy(d_arr, h_arr, size, cudaMemcpyHostToDevice); 
+    gettimeofday(&end, NULL);
+    htod = timecalc(start, end);
+    
+    gettimeofday(&start, NULL);
+    cudaMemcpy(h_arr, d_arr, size, cudaMemcpyDeviceToHost);
+    gettimeofday(&end, NULL);
+    dtoh = timecalc(start, end);
+
+    char sizeout[8];
+    switch (n/10) {
+        case 0:
+            sprintf(sizeout, "%0.0f", exp2((float)(n%10)));
+            break;
+        case 1:
+            sprintf(sizeout, "%0.0fK", exp2((float)(n%10)));
+            break;
+        case 2:
+            sprintf(sizeout, "%0.0fM", exp2((float)(n%10)));
+            break;
+        case 3:
+            sprintf(sizeout, "%0.0fG", exp2((float)(n%10)));
+            break;
+        default:
+            sprintf(sizeout, "err");
+    }
+    printf("%s, %0.2f, %0.2f\n", sizeout, htod, dtoh);
+
+    free(h_arr);
+    cudaFree(d_arr);
 }
